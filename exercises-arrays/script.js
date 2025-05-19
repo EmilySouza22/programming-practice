7e3be25fc85a55628df2aa3e2789b85d6164512d
EmilySouza22 <emilyseafonso@gmail.com>
2025-05-19 01:20:34 -0300

    Adiciona exercicios arrays

---
+++ exercises-arrays/script.js
@@ -0,0 +1,35 @@
+function exercise1(){
+    /* Fazer um algoritmo que declara um vetor vet com
+    6 elementos do tipo inteiro, inicializa vet com o
+    valor 2 e depois escreve o seu conteúdo. */
+    
+    let arr = [1,3,5,7,9,11];
+    for(let index = 0; index < arr.length ; index++){
+        arr[index] = 2;
+    }
+    console.log(arr);
+}
+
+function exercise2(){
+    /* Fazer um algoritmo que leia 5 valores numéricos
+    inteiros, os armazene em um vetor e os imprima. */
+    let arr = [];
+    let index = 0;
+    do {
+        let num = parseInt(prompt(`Insira um número inteiro`));
+        arr.push(num);
+        index++;
+    } while (index < 5);
+    alert(`Vetor: ${arr}`);
+}
+
+function exercise3(){
+    /* Fazer um algoritmo que leia 100 valores numéricos inteiros e armazene-os em um
+    vetor. Após, verifique – dentre estes valores - se existem valores iguais a 30. Se existirem,
+    escrever as posições em que estes valores estão armazenados. */
+}
+
+function exercise4(){
+    /* Faça um programa que leia 5 valores inteiros, armazeno-os em um vetor, calcule e apresente a soma
+    destes valores.  */
+}
