c3c62a6cce2f41d1b4594e439f3cac9aa26a9e62
EmilySouza22 <emilyseafonso@gmail.com>
2025-05-27 17:39:06 -0300

    Add exercício 2 de array

--- exercises-arrays/lista-2/script.js
+++ exercises-arrays/lista-2/script.js
@@ -1,5 +1,15 @@
+/* 
+
+    Lista de Exercícios Vetores 
+
+    ---> Exercicios_Complementares_02_Vetores_Matrizes
+
+*/
+
 function exercise1(){
-    /* 1. Leia 10 números e coloque em um vetor. Depois mostre esses números em ordem inversa da que foram lidos.  */
+    /* 1. Leia 10 números e coloque em um vetor. 
+    Depois mostre esses números em ordem inversa 
+    da que foram lidos.  */
 
     let arr = [];
 
@@ -18,4 +28,32 @@ function exercise1(){
 
     arr.reverse();
     console.log(arr);
+}
+
+function exercise2(){
+    /* 2. Leia 10 notas, coloque em um vetor e depois calcule a 
+    média delas. Mostre quantas notas estão abaixo da média, 
+    quantas estão na média e quantas estão acima da média.  */
+
+    const arr = [];
+    for(let i = 0; i < 10; i++){
+        let num = parseInt(prompt(`Digite um número`));
+        arr.push(num);
+    }
+
+    console.log(arr);
+
+    const abaixo= []; const media= []; const acima = [];
+
+    for(let i = 0; i < 10; i++){
+        if(arr[i] > 7){
+            acima.push(arr[i]);
+        }else if(arr[i] === 7){
+            media.push(arr[i]);
+        }else if(arr[i] < 7){
+            abaixo.push(arr[i]);
+        }
+    }
+
+    alert(`\n Abaixo da média: ${abaixo}; \n Na média: ${media}; \n Acima da média: ${acima};`);
 }
